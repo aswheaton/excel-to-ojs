ee82124d5131751cbf6f7b35cb78f7ff6bded8d9
aswheaton <alex.s.wheaton@gmail.com>
2022-05-21 18:42:37 +0100

    Finished importing authors and fixed file checking.

--- xml_convert.py
+++ xml_convert.py
@@ -11,6 +11,9 @@ def view_column(dataframe, column_name):
     for row in range(1, dataframe.shape[0]):
         print(row, dataframe[row][col])
 
+def indent():
+    return(indent_level * 8 * " ")
+
 dataframe = np.loadtxt("database/Pachy.csv",dtype=str,delimiter="	")
 
 # The column names are ['Authors' 'Title' 'Sec_title' 'Year_pub' 'Section' 'Volume' 'Page_start' 'Page_end' 'Abstract' 'EnteredBy' 'FileName' '']
@@ -32,20 +35,89 @@ for volume in range(1, 41):
     if not os.path.exists("bepress_xml/"+str(volume)+"/1/"):
         os.makedirs("bepress_xml/"+str(volume)+"/1/", mode=0o666)
 
-
-
 for row in range(1, dataframe.shape[0]):
-
     #Get essential info out of the file.
     volume_no = int(dataframe[row][5])
     issue_no = 1
-
 
     # Devise a new filename for the file.
     old_filename = dataframe[row][10]
     new_filename = old_filename
     # Copy the file to its new directory, if not already present.
     src = os.path.join("database/articles/", old_filename)
-    dst = os.path.join("bepress_xml/", volume_no, issue_no, new_filename)
-    if not os.isfile(dst):
-        shutil.copy2(src, dst)
+    dst = os.path.join("bepress_xml/", str(volume_no), str(issue_no), new_filename)
+    if not os.path.exists(dst):
+        shutil.copy2(src, dst) # TODO: Add PDFs to database directory!
+
+
+f = open('metadata.xml', 'w')
+
+indent_level = 0
+
+# Begin writing to the metadata document.
+f.write("<?xml version='1.0' encoding='utf-8' ?>\n")
+indent_level += 1
+f.write(indent()+"<documents>\n")
+indent_level += 1
+
+
+for row in range(1, 5):#dataframe.shape[0]):
+
+    title = dataframe[row][1]
+    f.write(indent()+"<titles>\n")
+    indent_level += 1
+    f.write(indent()+"<title>\n")
+    indent_level += 1
+    f.write(indent()+title+"\n")
+    indent_level -= 1
+    f.write(indent()+"</titles>\n")
+    indent_level -= 1
+    f.write(indent()+"</title>\n")
+    indent_level -= 1
+
+    volume_no = int(dataframe[row][5])
+    pubyear = dataframe[row][3]
+    if volume_no % 2 != 0:
+        pubdate = str(pubyear)+"-06-30:T00:00:00-07:00" # Why this particular date format?
+    elif volume_no % 2 == 0:
+        pubdate = str(pubyear)+"-12-31:T00:00:00-07:00" # Why this particular date format?
+
+    f.write(indent()+"<publication-date>" + pubdate + "</publication-date>" + "\n")
+    f.write(indent()+"<state>published</state>\n")
+
+    f.write(indent()+"<authors>\n")
+    indent_level += 1
+
+    authors = dataframe[row][0]
+    author_list = authors.split(delimiter=";")
+    for author in author_list:
+        f.write(indent()+"<author>\n")
+        indent_level += 1
+        f.write(indent()+"<lname>"+author.split(" ")[0]+"</lname>\n")
+        f.write(indent()+"<fname>"+author.split(" ")[-1]+"</fname>\n")
+        try:
+            f.write(indent()+"<mname>"+author.split(" ")[1]+"</mname>\n")
+        except IndexError:
+            pass
+        indent_level -= 1
+        f.write(indent()+"</author>\n")
+
+    indent_level -= 1
+    f.write(indent()+"</authors>\n")
+
+    abstract = dataframe[row][8]
+    f.write(indent()+"<abstract>"+abstract+"</abstract>\n")
+
+    section = dataframe[row][4]
+    f.write(indent()+"<document-type>"+section+"</document-type>\n")
+    f.write(indent()+"<type>article</type>\n")
+
+    page_start, page_end = dataframe[row][6], dataframe[row][7]
+    f.write(indent()+"<fpage>"+page_start+"</fpage>\n")
+    f.write(indent()+"<lpage>"+page_end+"</lpage>\n")
+
+indent_level -= 1
+f.write(indent()+"</document>\n")
+
+indent_level -=1
+f.write(indent(indent_level)+"</documents>")
