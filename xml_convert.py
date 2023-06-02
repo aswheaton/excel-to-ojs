ccaff46828136317516e5347ca08de376e960874
aswheaton <alex.s.wheaton@gmail.com>
2023-06-02 09:28:10 +0100

    Fixes for logging and pagination.

--- xml_convert.py
+++ xml_convert.py
@@ -2,6 +2,53 @@ import os
 import numpy as np
 import shutil
 
+def view_headers(dataframe):
+
+    print(dataframe[0,:])
+    # log("Viewing dataframe headers, they are {}.\n".format(dataframe[0,:]))
+
+def view_column(dataframe, column_name):
+    # Get index associated with the column name:
+    col = np.where(dataframe[0,:]==column_name)
+    for row in range(1, dataframe.shape[0]):
+        print(row, dataframe[row][col])
+
+def indent():
+
+    return(indent_level * "	")
+
+def open_tag(tag_name):
+
+    global indent_level
+    xml_file.write(indent()+"<"+tag_name+">\n")
+    indent_level += 1
+
+def close_tag(tag_name):
+
+    global indent_level
+    indent_level -= 1
+    xml_file.write(indent()+"</"+tag_name+">\n")
+
+def tag(tag_name, tag_content, **kwargs):
+
+    xml_file.write(indent())
+    # Open the tag with any keyword arguments.
+    xml_file.write("<"+tag_name)
+    for key, value in kwargs.items():
+        xml_file.write(" " + key + "=\"" + value + "\"")
+    xml_file.write(">")
+    # Fill the tag with content.
+    xml_file.write(tag_content)
+    # Close the tag and move to newline.
+    xml_file.write("</"+tag_name+">\n")
+
+logfile = open("log", "w")
+
+def log(string):
+    print(string)
+    global logfile
+    logfile.write(string + "\n")
+
 journal_name = "Pachyderm"
 
 pubdates = {
@@ -49,65 +96,72 @@ pubdates = {
     }
 
 author_emails = {
-    "Ghosh"         : "Omitted from public repository.",
-    "Rookmaaker"    : "Omitted from public repository.",
-    "Nishihara"     : "Omitted from public repository.",
-    "Hillman"       : "Omitted from public repository.",
+    "African Elephant Specialist Group" : "Omitted from public repository.",
+    "Adcock" : "Omitted from public repository.",
+    "Amin" : "Omitted from public repository.",
+    "Anonymous" : "Omitted from public repository.",
+    "Barnes" : "Omitted from public repository.",
+    "Bouché" : "Omitted from public repository.",
+    "Brett" : "Omitted from public repository.",
+    "Brooks" : "Omitted from public repository.",
+    "Choudhury" : "Omitted from public repository.",
+    "Craig" : "Omitted from public repository.",
+    "Cumming" : "Omitted from public repository.",
+    "Danquah" : "Omitted from public repository.",
+    "Douglas" : "Omitted from public repository.",
+    "Dublin" : "Omitted from public repository.",
+    "Emslie" : "Omitted from public repository.",
+    "Gakuya" : "Omitted from public repository.",
+    "Garaï" : "Omitted from public repository.",
+    "Ghosh" : "Omitted from public repository.",
+    "Hema" : "Omitted from public repository.",
+    "Hillman" : "Omitted from public repository.",
     "Hillman-Smith" : "Omitted from public repository.",
-    "Stiles"        : "Omitted from public repository.",
-    "Vigne"         : "Omitted from public repository.",
-    "Schulte"       : "Omitted from public repository.",
-    "Lindsay"       : "Omitted from public repository.",
-    "Hoare"         : "Omitted from public repository.",
-    "Potter"        : "Omitted from public repository."
+    "Hoare" : "Omitted from public repository.",
+    "Hoare" : "Omitted from public repository.",
+    "Jachmann" : "Omitted from public repository.",
+    "Leader" : "Omitted from public repository.",
+    "Leader-Williams" : "Omitted from public repository.",
+    "Lindsay" : "Omitted from public repository.",
+    "Lindsay" : "Omitted from public repository.",
+    "Litoroh" : "Omitted from public repository.",
+    "Malpas" : "Omitted from public repository.",
+    "Martin" : "Omitted from public repository.",
+    "Milliken" : "Omitted from public repository.",
+    "Mubalama" : "Omitted from public repository.",
+    "Mulama" : "Omitted from public repository.",
+    "Nishihara" : "Omitted from public repository.",
+    "Okita" : "Omitted from public repository.",
+    "Olivier" : "Omitted from public repository.",
+    "Omondi" : "Omitted from public repository.",
+    "Osborn" : "Omitted from public repository.",
+    "Osofsky" : "Omitted from public repository.",
+    "Potter" : "Omitted from public repository.",
+    "Reilly" : "Omitted from public repository.",
+    "Rookmaaker" : "Omitted from public repository.",
+    "Rookmaaker" : "Omitted from public repository.",
+    "Schulte" : "Omitted from public repository.",
+    "Sebogo" : "Omitted from public repository.",
+    "Spinage" : "Omitted from public repository.",
+    "Stiles" : "Omitted from public repository.",
+    "Stiles" : "Omitted from public repository.",
+    "Strien" : "Omitted from public repository.",
+    "Talukdar" : "Omitted from public repository.",
+    "Taylor" : "Omitted from public repository.",
+    "Thouless" : "Omitted from public repository.",
+    "Toit" : "Omitted from public repository.",
+    "du Toit" : "Omitted from public repository.",
+    "Turkalo" : "Omitted from public repository.",
+    "Vigne" : "Omitted from public repository.",
+    "Walker" : "Omitted from public repository.",
+    "Western" : "Omitted from public repository.",
+    "Whyte" : "Omitted from public repository."
     }
 
-log = open("log", "w")
-
-dataframe = np.loadtxt("database/Pachy.csv",dtype=str,delimiter="	")
-log.write("Successfully loaded CSV file into dataframe.\n")
+dataframe = np.loadtxt("database/Pachy.csv", dtype=str, delimiter="	")
+log("Successfully loaded CSV file into dataframe.")
 
 aggregate_authors = []
-
-def view_headers(dataframe):
-
-    print(dataframe[0,:])
-    # log.write("Viewing dataframe headers, they are {}.\n".format(dataframe[0,:]))
-
-def view_column(dataframe, column_name):
-    # Get index associated with the column name:
-    col = np.where(dataframe[0,:]==column_name)
-    for row in range(1, dataframe.shape[0]):
-        print(row, dataframe[row][col])
-
-def indent():
-
-    return(indent_level * "	")
-
-def open_tag(tag_name):
-
-    global indent_level
-    xml_file.write(indent()+"<"+tag_name+">\n")
-    indent_level += 1
-
-def close_tag(tag_name):
-
-    global indent_level
-    indent_level -= 1
-    xml_file.write(indent()+"</"+tag_name+">\n")
-
-def tag(tag_name, tag_content, **kwargs):
-
-    xml_file.write(indent())
-    # Open the tag with any keyword arguments.
-    xml_file.write("<"+tag_name)
-    for key, value in kwargs.items():
-        xml_file.write(" " + key + "=\"" + value + "\"")
-    xml_file.write(">")
-    # Fill the tag with content.
-    xml_file.write(tag_content)
-    # Close the tag and move to newline.
-    xml_file.write("</"+tag_name+">\n")
 
 """
 Notes:
@@ -120,7 +174,7 @@ TODO: It appears that some files are referenced by multiple rows, for short arti
 TODO: Not clear what purpose the final column serves. FIXED!
 TODO: Some filenames in database have capital P, although no such file exists. FIXED, kinda!
 TODO: Check logic of directory structure creation for missing file in row 1. WORKS!
-TODO: Add section names (either <type> or <document-type>) and <galley> tags.
+TODO: Add section names (either <type> or <document-type>) and <galley> tags. WORKS!
 """
 
 for row in range(1, dataframe.shape[0]):
@@ -129,7 +183,7 @@ for row in range(1, dataframe.shape[0]):
     issue_no = 1
 
     if volume_no == 42:
-        log.write("Volume number exceeds available galleys! Stopping conversion.\n")
+        log("Volume number exceeds available galleys! Stopping conversion.")
         break
 
     # If this is the first article in the first issue, start an article counter at 1.
@@ -149,18 +203,42 @@ for row in range(1, dataframe.shape[0]):
     article_path = os.path.join("bepress_xml", journal_name, str(volume_no), str(issue_no), str(article_no))
     if not os.path.exists(article_path):
         os.makedirs(article_path, mode=0o777)
-        log.write("Created directory {} with permissions 777.\n".format(article_path))
+        log("Created directory {} with permissions 777.".format(article_path))
 
     # Copy the file to its new directory, if not already present.
     src = os.path.join("database/articles/", old_filename.lower()) # Actual filenames do not contain captial letters.
     dst = os.path.join(article_path, new_filename)
 
+    using_inclusive_pdf = False
+
+    if not os.path.exists(src):
+        log("WARNING: Problem fetching source in row {}. Could not find a file {}, looking for inclusive PDFs.".format(row+1, src))
+        # Try to find a file whose page numbers are inclusive of the article.
+        page_start, page_end = dataframe[row][6], dataframe[row][7]
+        # Get all the filenames belonging to the current issue.
+        issue_pdfs = []
+        for filename in os.listdir("database/articles/"):
+            if int(filename.split(".")[0][5:]) == volume_no:
+                issue_pdfs.append(filename)
+        # Get all the filenames in the current issue with page numbers inclusive of the metadata page range.
+        inclusive_pdfs = []
+        for filename in issue_pdfs:
+            pdf_page_start, pdf_page_end = int(filename.split(".")[2]), int(filename.split(".")[3])
+            if (pdf_page_start <= int(page_start)) and (pdf_page_end >= int(page_end)):
+                inclusive_pdfs.append(filename)
+        if len(inclusive_pdfs) == 1:
+            using_inclusive_pdf = True
+            old_filename = inclusive_pdfs[0]
+            new_filename = inclusive_pdfs[0].lower()
+            src = os.path.join("database/articles/", new_filename)
+            log("Found inclusive PDF {}.".format(src))
+
     if not os.path.exists(dst):
         try:
             shutil.copy2(src, dst)
-            log.write("Copied file {} to {}.\n".format(src, dst))
+            log("Copied file {} to {}.".format(src, dst))
         except FileNotFoundError:
-            log.write("Error in row {}. Could not find a file at {}, writing metadata anyway.\n".format(row+1, src))
+            log("ERROR: in row {}. Could not find a file at {}, writing metadata anyway.".format(row+1, src))
 
     # Now create the requisite metadata file for the article.
     xml_file_path = os.path.join(article_path, "metadata.xml")
@@ -234,6 +312,15 @@ for row in range(1, dataframe.shape[0]):
     tag("type", "article")
 
     page_start, page_end = dataframe[row][6], dataframe[row][7]
+    pdf_page_start, pdf_page_end = int(new_filename.split(".")[2]), int(new_filename.split(".")[3])
+
+    if pdf_page_start != int(page_start) or pdf_page_end != int(page_end):
+        log(f"WARNING: in row {row}! Mismatched pagination {pdf_page_start}-{pdf_page_end} with metadata {int(page_start)}-{int(page_end)} for file {src}.")
+        if not using_inclusive_pdf:
+            dataframe[row][6], dataframe[row][7] = str(int(new_filename.split(".")[2])), str(int(new_filename.split(".")[3]))
+            page_start, page_end = dataframe[row][6], dataframe[row][7]
+            log(f"Using PDF pagination for metadata in row {row} and updating dataframe.")
+
     tag("fpage", page_start)
     tag("lpage", page_end)
 
@@ -242,6 +329,10 @@ for row in range(1, dataframe.shape[0]):
 
     xml_file.close()
 
+np.savetxt('database/Pachy.tsv', dataframe, fmt='%s', delimiter='	', newline='\n', header='', footer='', comments='# ', encoding=None)
+
+
+"""
 for volume_no in range(1,42):
 
     issue_no = 1
@@ -254,7 +345,7 @@ for volume_no in range(1,42):
     src = os.path.join("database/covers/", old_cover_filename)
     dst = os.path.join("bepress_xml/", journal_name, str(volume_no), str(issue_no), "cover.jpg")
     shutil.copy2(src, dst)
-    log.write("Copied {} to {}.\n".format(src, dst))
+    log("Copied {} to {}.".format(src, dst))
 
 publication_threshold = 3
 
@@ -271,22 +362,22 @@ for i in range(len(unique_authors)):
         reduced_unique_authors.append(unique_authors[i])
 
 # for i in range(len(reduced_unique_authors)):
-#     log.write("Author {} is a contributor on {} articles.\n".format(reduced_unique_authors[i], aggregate_authors.count(reduced_unique_authors[i])))
+#     log("Author {} is a contributor on {} articles.".format(reduced_unique_authors[i], aggregate_authors.count(reduced_unique_authors[i])))
 
 contributions = 0
 for row in range(1, dataframe.shape[0]):
     for author in reduced_unique_authors:
         if author in dataframe[row,0]:
-            # log.write("Found {} in {}.\n".format(author, dataframe[row,0]))
+            # log("Found {} in {}.".format(author, dataframe[row,0]))
             contributions +=1
             break
 
-log.write("Found {} unique authors who have published {} or more papers.\n".format(len(reduced_unique_authors), publication_threshold))
-log.write("The authors are:\n")
+log("Found {} unique authors who have published {} or more papers.".format(len(reduced_unique_authors), publication_threshold))
+log("The authors are:")
 for author in reduced_unique_authors:
-    log.write("{}; \n".format(author))
-log.write("{}% of contributions are from these {} individuals.\n".format(contributions/row*100, len(reduced_unique_authors)))
-
+    log("{}; ".format(author))
+log("{}% of contributions are from these {} individuals.".format(contributions/row*100, len(reduced_unique_authors)))
+"""
 """
 Found 160 unique authors who have published 2 or more papers.
 79.50% of contributions are from these 160 individuals.
@@ -318,4 +409,4 @@ Found 17 unique authors who have published 10 or more papers.
 10+ papers -> 17 unique authors comprising 40.73% of all articles.
 """
 
-log.close()
+logfile.close()
